cdacb830a4711339411a335b248302e8044baaf8
Vitor Jordao <srvitorlol@gmail.com>
2020-06-07 19:47:21 -0300

    Add who we are

--- gulpfile.js
+++ gulpfile.js
@@ -87,6 +87,19 @@ cssOptimizer('Terms', [
     "./Screens/Terms/*.css",
 ]);
 
+// WhoWeAre
+javascriptOptimizer('WhoWeAre', [
+    "./Design_Thinking/js/menu.js",
+]);
+cssOptimizer('WhoWeAre', [
+    "./Design_Thinking/css/basics.css",
+    "./Design_Thinking/css/buttons.css",
+    "./Design_Thinking/css/header.css",
+    "./Design_Thinking/css/cards.css",
+    "./Design_Thinking/css/footer.css",
+    "./Screens/WhoWeAre/*.css",
+]);
+
 // Watch
 gulp.task('watch', function () {
     gulp.watch(['Design_Thinking/**/*.*', 'Screens/**/*.*', '!Screens/**/prod/**/*.*'], gulp.series(tasks));
