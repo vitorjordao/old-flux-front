c0b3bacf0d0f333a6fcb4659078273fa47ad11ae
Vitor Jordao <srvitorlol@gmail.com>
2020-06-07 09:08:24 -0300

    Organize gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -5,76 +5,76 @@ var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 var minifyCss = require('gulp-minify-css');
 
-// Minify an Concat home CSS
-gulp.task('home-minify-css', function () {
-    return gulp.src([
-        "./Design_Thinking/css/basics.css",
-        "./Design_Thinking/css/buttons.css",
-        "./Design_Thinking/css/header.css",
-        "./Design_Thinking/css/cards.css",
-        "./Design_Thinking/css/forms.css",
-        "./Design_Thinking/css/footer.css",
-        "./Screens/Home/*.css",
-    ])
-        .pipe(plumber())
-        .pipe(minifyCss())
-        .pipe(concat("style.css"))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(plumber.stop())
-        .pipe(gulp.dest("./Screens/Home/prod/css/"));
-});
+let tasks = [];
 
-// Minify an Concat home JS
-gulp.task('home-minify-js', function () {
-    return gulp.src([
-        "./Design_Thinking/js/menu.js",
-        "./Design_Thinking/js/auth.js",
-    ])
-        .pipe(plumber())
-        .pipe(uglify())
-        .pipe(concat("main.js"))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(plumber.stop())
-        .pipe(gulp.dest("./Screens/Home/prod/js/"));
-});
+function javascriptOptimizer(name = '', modules = []) {
+    const taskName = `JS-${name}`;
 
-// Minify an Concat auth CSS
-gulp.task('auth-minify-css', function () {
-    return gulp.src([
-        "./Design_Thinking/css/basics.css",
-        "./Design_Thinking/css/buttons.css",
-        "./Design_Thinking/css/header.css",
-        "./Design_Thinking/css/cards.css",
-        "./Design_Thinking/css/forms.css",
-        "./Design_Thinking/css/footer.css",
-        "./Screens/Auth/*.css",
-    ])
-        .pipe(plumber())
-        .pipe(minifyCss())
-        .pipe(concat("style.css"))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(plumber.stop())
-        .pipe(gulp.dest("./Screens/Auth/prod/css/"));
-});
+    tasks.push(taskName);
 
-// Minify an Concat auth JS
-gulp.task('auth-minify-js', function () {
-    return gulp.src([
-        "./Design_Thinking/js/menu.js",
-        "./Design_Thinking/js/auth.js",
-    ])
-        .pipe(plumber())
-        .pipe(uglify())
-        .pipe(concat("main.js"))
-        .pipe(rename({ suffix: '.min' }))
-        .pipe(plumber.stop())
-        .pipe(gulp.dest("./Screens/Auth/prod/js/"));
-});
+    // Minify an Concat JS
+    gulp.task(taskName, function () {
+        return gulp.src(modules)
+            .pipe(plumber())
+            .pipe(uglify())
+            .pipe(concat("main.js"))
+            .pipe(rename({ suffix: '.min' }))
+            .pipe(plumber.stop())
+            .pipe(gulp.dest(`./Screens/${name}/prod/js/`));
+    });
+}
+
+function cssOptimizer(name = '', modules = []) {
+    const taskName = `CSS-${name}`;
+
+    tasks.push(taskName);
+
+    // Minify an Concat CSS
+    gulp.task(taskName, function () {
+        return gulp.src(modules)
+                .pipe(plumber())
+                .pipe(minifyCss())
+                .pipe(concat("style.css"))
+                .pipe(rename({ suffix: '.min' }))
+                .pipe(plumber.stop())
+            .pipe(gulp.dest(`./Screens/${name}/prod/css/`));
+    });
+}
+
+// HOME
+javascriptOptimizer('Home', [
+    "./Design_Thinking/js/menu.js",
+    "./Design_Thinking/js/auth.js",
+]);
+cssOptimizer('Home', [
+    "./Design_Thinking/css/basics.css",
+    "./Design_Thinking/css/buttons.css",
+    "./Design_Thinking/css/header.css",
+    "./Design_Thinking/css/cards.css",
+    "./Design_Thinking/css/forms.css",
+    "./Design_Thinking/css/footer.css",
+    "./Screens/Home/*.css",
+]);
+
+// AUTH
+javascriptOptimizer('Auth', [
+    "./Design_Thinking/js/menu.js",
+    "./Design_Thinking/js/auth.js",
+]);
+cssOptimizer('Auth', [
+    "./Design_Thinking/css/basics.css",
+    "./Design_Thinking/css/buttons.css",
+    "./Design_Thinking/css/header.css",
+    "./Design_Thinking/css/cards.css",
+    "./Design_Thinking/css/forms.css",
+    "./Design_Thinking/css/footer.css",
+    "./Screens/Auth/*.css",
+]);
 
 // Watch
 gulp.task('watch', function () {
-    gulp.watch([cssSrc], ['home-minify-css', 'home-minify-js', 'auth-minify-css', 'auth-minify-js']);
+    gulp.watch([cssSrc], tasks);
 });
 
 // Default
-gulp.task('default', gulp.series(['home-minify-css', 'home-minify-js', 'auth-minify-css', 'auth-minify-js']));
+gulp.task('default', gulp.series(tasks));
